276526006a18abb6d1655e16043946ee86407c44
Dian Rizky <rizdian229@gmail.com>
2024-10-04 10:55:22 +0700

    Landing 1.1

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,52 +1,50 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary" :class="{ 'sticky': isSticky }">
     <div class="container">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="/media/hero/custiket.png" alt="CusTicket" width="100" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/home">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/tiket">Tiket</router-link>
-            </li>
-          </ul>
+      <a class="navbar-brand" href="#">
+        <img src="/media/hero/custiket.png" alt="CusTicket" width="80" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/home">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/tiket">Tiket</router-link>
+          </li>
+        </ul>
 
-          <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/keranjang">
-                Keranjang
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-bag"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"
-                  />
-                </svg>
-                <span class="badge badge-success ml-2">{{ updateKeranjang ? updateKeranjang.length : jumlah_pesanans.length }}</span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/keranjang">
+              Keranjang
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-bag"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"
+                />
+              </svg>
+              <span class="badge badge-success ms-2">{{ updateKeranjang ? updateKeranjang.length : jumlah_pesanans.length }}</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
@@ -54,10 +52,12 @@
 
 <script>
 import axios from "axios";
+
 export default {
   name: "Navbar",
   data() {
     return {
+      isSticky: false,
       jumlah_pesanans: [],
     };
   },
@@ -66,16 +66,40 @@ export default {
     setJumlah(data) {
       this.jumlah_pesanans = data;
     },
+    handleScroll() {
+      this.isSticky = window.scrollY > 0;
+    },
   },
   mounted() {
     axios
       .get("http://localhost:3000/keranjangs")
       .then((response) => this.setJumlah(response.data))
       .catch((error) => console.log(error));
+
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
 
+<style lang="scss" scoped>
+.navbar {
+  transition: background-color 0.3s ease, box-shadow 0.3s ease;
 
-<style>
+  &.sticky {
+    background-color: #ffffff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    z-index: 1000;
+  }
+}
+
+.nav-link {
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: #28a745;
+  }
+}
 </style>
--- resources/js/pages/HomeView.vue
+++ resources/js/pages/HomeView.vue
@@ -1,58 +1,36 @@
+<!-- src/views/HomeView.vue -->
 <template>
-  <div class="home">
-    <Navbar />
-    <div class="container">
-      <Hero />
-
-      <div class="row mt-4">
-        <div class="col">
-          <h2>Best <strong>Concert</strong></h2>
-        </div>
-        <div class="col">
-          <router-link to="/tiket" class="btn btn-success float-right"
-            >Lihat semua</router-link
-          >
-        </div>
-      </div>
-
-      <div class="row mb-4">
-        <div class="col-md-4 mt-4" v-for="product in products" :key="product.id">
-          <CartProduct :product="product"/>
-        </div>
+  <LayoutLanding>
+    <div class="container mt-4">
+      <Carousel />
+      <BestConcert />
+      <div class="row">
+        <HomeCard />
+        <HomeCard />
+        <!-- Tambahkan lebih banyak HomeCard jika diperlukan -->
       </div>
     </div>
-  </div>
+  </LayoutLanding>
 </template>
 
+
 <script>
-// @ is an alias to /src
-import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import CartProduct from "@/components/CartProduct.vue";
-import axios from "axios";
+import LayoutLanding from "@/layouts/LayoutLanding.vue";
+import Carousel from "@/components/Carousel.vue";
+import BestConcert from "@/components/BestConcert.vue";
+import HomeCard from "@/components/HomeCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    Navbar,
-    Hero,
-    CartProduct,
-  },
-  data() {
-    return {
-      products: [],
-    };
-  },
-  methods: {
-    setProducts(data) {
-      this.products = data;
-    },
-  },
-  mounted() {
-    axios
-      .get("http://localhost:3000/best-products")
-      .then((response) => this.setProducts(response.data))
-      .catch((error) => console.log(error))
+    LayoutLanding,
+    Carousel,
+    BestConcert,
+    HomeCard,
   },
 };
 </script>
+
+<style scoped>
+/* Tambahkan style khusus untuk HomeView jika perlu */
+</style>
