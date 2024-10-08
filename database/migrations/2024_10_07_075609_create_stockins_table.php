a753a30239255a02e9374cff74c2c7aa7862be0a
GustiSangAji <gustavdrk@gmail.com>
2024-10-08 11:12:28 +0700

    TRIGGER TAMBAH pada STOCKINS

--- database/migrations/2024_10_07_075609_create_stockins_table.php
+++ database/migrations/2024_10_07_075609_create_stockins_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -11,16 +12,31 @@ return new class extends Migration
         Schema::create('stockins', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->string('kode_tiket')->unique();
+            $table->string('kode_tiket');
             $table->integer('jumlah');
             $table->text('deskripsi');
-            $table->datetime('datetime'); // Tanggal event
+            $table->datetime('event_datetime'); // Tanggal event
             $table->timestamps();
         });
+
+        // Menambahkan trigger menggunakan raw SQL
+        DB::unprepared('
+            CREATE TRIGGER trigger_tambah
+            AFTER INSERT ON stockins
+            FOR EACH ROW
+            BEGIN
+                UPDATE tikets SET quantity = quantity + NEW.jumlah
+                WHERE kode_tiket = NEW.kode_tiket;
+            END
+        ');
     }
 
     public function down(): void
     {
+        // Drop trigger sebelum menghapus tabel
+        DB::unprepared('DROP TRIGGER IF EXISTS trigger_tambah');
+
         Schema::dropIfExists('stockins');
     }
 };
+
