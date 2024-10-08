978a07538993f47b83ec3b63f9f7157f5ca4033d
HellerZ3 <gilarfirnanda@gmail.com>
2024-10-08 08:41:04 +0700

    factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {   
-        \App\Models\Order::factory(10)->create();
         $this->call([
             RoleSeeder::class,
             PermissionSeeder::class,
